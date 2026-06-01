da6a3c6d820db63d9430c9fa67ad026a4acd9ac6
Fan Pu <46126257+Fan-Pu@users.noreply.github.com>
2026-06-01 12:57:44 -0500

    save

--- main.py
+++ main.py
@@ -554,7 +554,7 @@ def run_scenario(config_path: str) -> Dict[str, object]:
         for _ in range(2):
             print()
 
-        if debug_step_index >= 10:
+        if debug_step_index >= 6:
             debugpy.breakpoint()
 
 
