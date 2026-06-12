32f65c3e7ee885cca866bf8c10a5649af224ca32
Fan Pu <46126257+Fan-Pu@users.noreply.github.com>
2026-06-11 22:48:08 -0500

    Update Helper.py

--- Helper.py
+++ Helper.py
@@ -367,7 +367,7 @@ def explore_world(sim, location=0, heading=0, elevation=0):
         rgb = np.array(state.rgb, copy=False)
         current_vp_id = viewpoint_index_by_vp_label[str(state.location.viewpointId)]
         print(
-            f"current vp: {current_vp_id}, elevation: {math.degrees(state.elevation):.1f} deg, heading: {math.degrees(state.heading):.1f} deg"
+            f"current vp id: {current_vp_id}, long id: {state.location.viewpointId}, elevation: {math.degrees(state.elevation):.1f} deg, heading: {math.degrees(state.heading):.1f} deg"
         )
         for idx, loc in enumerate(locations[1:]):
             # Draw actions on the screen
